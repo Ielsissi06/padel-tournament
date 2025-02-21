543b53643e4bb4ef2fdd00b67d1c47d555fd40ef
Ielsissi06 <ielsissi3@gatech.edu>
2025-02-21 12:49:19 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxHNbs6g-2FMNlCn-AMZPBxV3xDF7rs9iYBJ3tUMQT-OIAIMuE5uGyQwCIm7h17FpgS/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thank you! Your form has been submitted successfully. Redirecting to payment link..." ))
+    .then(() => { window.location.href = 'https://buy.stripe.com/6oEg28fh2c1T7uMbIJ'; })
+    .catch(error => console.error('Error!', error.message))
+})
